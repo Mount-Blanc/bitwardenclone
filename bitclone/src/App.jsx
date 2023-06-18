6b9c355e1819e5c1188c0d9718bbac4b0026e1cf
Mount-Blanc <mountblanc100@gmail.com>
2023-06-18 13:28:01 -0700

    added basic elements to header

--- bitclone/src/App.jsx
+++ bitclone/src/App.jsx
@@ -1,33 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+     <header>
+      <div className='header-container'>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>bitwarden</h1>
+        <ul>
+          <li><a href=''>Personal</a></li>
+          <li><a href=''>Business</a></li>
+          <li><a href=''>Developers</a></li>
+          <li><a href=''>Pricing</a></li>
+          <li><a href=''>Help</a></li>
+
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div>
+        <ul>
+        <li><a href=''>Business Sales</a></li>
+        <li><a href=''>Get Started</a></li>
+        <li><a href=''>Log In</a></li>
+
+      </ul>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </div>
+     </header>
     </>
   )
 }
