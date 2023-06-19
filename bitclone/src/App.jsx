257846107f575d3a829a7c0167b38694f53d439f
Mount-Blanc <mountblanc100@gmail.com>
2023-06-19 10:31:54 -0700

    added section

--- bitclone/src/App.jsx
+++ bitclone/src/App.jsx
@@ -29,6 +29,7 @@ function App() {
       </div>
       </div>
      </header>
+
      <section>
       <div>
         <h1>Move fast and securely with the password manager trusted
@@ -40,6 +41,20 @@ function App() {
         <a href=''>Get Started Today</a>
         <a href=''>View Plans & Pricing</a>
 
+      </div>
+     </section>
+
+     <section>
+      <div>
+        <div>
+          <h2>Everything you need out of a password manager</h2>
+          <div>
+            <div>Easy</div>
+            <div>Convienent</div>
+            <div>Secure</div>
+
+          </div>
+        </div>
       </div>
      </section>
     </>
